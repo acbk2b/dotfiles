d99f63b92159c8857930e52a08b822da82617146
acbk2b <acbecker27@gmail.com>
2020-03-29 01:34:27 -0500

    Fixed spotifyd start script

--- scripts/start_spotifyd.sh
+++ scripts/start_spotifyd.sh
@@ -1,3 +1,8 @@
-bw unlock
+# !/bin/bash
+
+# export bitwarden session key
+$( bw unlock | grep -E "export BW_SESSION=.+" -o )
+# pass spotify password from bitwarden to spotifyd
 spotifyd -P 'bw get password spotify'
+# start spotifyd dameon as system service
 systemctl start spotifyd
