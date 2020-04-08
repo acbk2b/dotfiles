acb422c356430b3aff9f430892b259f52f04d7ec
acbk2b <acbecker27@gmail.com>
2020-04-08 14:08:58 -0500

    Improved death_to script

--- scripts/death_to.sh
+++ scripts/death_to.sh
@@ -1,1 +1,11 @@
-kill $( ps -d | grep $1 | grep -E "[[:digit:]]{3,5}" -o )
+#! /bin/bash
+
+PROCESSES=$(ps -d | grep -w $1 | grep -E "[[:digit:]]{3,5}" -o)
+
+if [ "$PROCESSES" != "" ];
+then
+    kill $PROCESSES
+    echo "Killed all processes for $1!"
+else
+    echo "No processes to kill!"
+fi
