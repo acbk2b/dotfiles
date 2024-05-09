c6f4e5bb53fd01a015991682cb4db0969fa07c83
Alex Becker <alex.becker@mastercard.com>
2024-05-09 15:13:20 -0500

    Add autocommand for .MCOM file extensions

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,8 +1,17 @@
+-- File type overrides
+
 -- Set Jenkinsfiles to groovy FileType
 vim.api.nvim_create_autocmd({"BufEnter"}, {
     pattern = "Jenkinsfile*",
     command = "setfiletype groovy"
 })
+
+-- Set *MCOM to csv FileType
+vim.api.nvim_create_autocmd({"BufEnter"}, {
+    pattern = "*.MCOM",
+    command = "setfiletype csv"
+})
+
 
 -- Turn on spell check for .md and .wiki files
 vim.api.nvim_create_autocmd({"BufEnter"}, {
