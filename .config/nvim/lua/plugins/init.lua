07d0c7631c57b4ac2038756e6e2226c5810efbaf
Alex Becker <alex.becker@mastercard.com>
2024-06-14 15:58:27 -0500

    Change from coc to nvim lsp for completions

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,25 +3,30 @@
 -- Load Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    { 'neoclide/coc.nvim', branch = 'release', },
-    { 'nvim-tree/nvim-tree.lua', lazy = true, },
-    { 'tpope/vim-commentary' },
+    { 'christoomey/vim-tmux-navigator' },
     { 'dracula/vim', lazy = true, name = 'dracula' },
     { 'easymotion/vim-easymotion' },
     { 'mechatroner/rainbow_csv' },
-    { 'tpope/vim-surround' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'L3MON4D3/LuaSnip' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'nvim-tree/nvim-tree.lua', lazy = true, },
     {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
@@ -30,18 +35,20 @@ local plugins = {
             configs.setup(require('plugins/treesitter'))
         end
     },
+    { 'tpope/vim-commentary' },
+    { 'tpope/vim-surround' },
+    { 'vim-airline/vim-airline' },
     { 'vimwiki/vimwiki' },
-    { 'christoomey/vim-tmux-navigator' },
-    { 'vim-airline/vim-airline' }
 }
 
+-- Add any options for Lazy here
 local opts = {}
 
 require('lazy').setup(plugins, opts)
 
 -- Plugin configuration setup
 require('plugins/airline')
-require('plugins/coc-config')
+require('lsp')
 require('plugins/nvim-tree')
 require('plugins/vimwiki')
 -- Load WSL config if running on WSL
