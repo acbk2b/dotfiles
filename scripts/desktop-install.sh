90a327a3b37921ba07c93c674261b650791df3d0
Alex Becker <alex@beckerlab.net>
2026-09-09 19:52:48 -0500

    fix: conditional for desktop install script

--- scripts/desktop-install.sh
+++ scripts/desktop-install.sh
@@ -6,40 +6,35 @@ if [ -f /etc/os-release ]; then
 	. /etc/os-release
 fi
 
-# Basic desktop check (can be improved based on preference)
-IS_DESKTOP=false
-if [[ -n "$DISPLAY" ]] || [[ -n "$WAYLAND_DISPLAY" ]]; then
-	IS_DESKTOP=true
-fi
-
-if [ "$IS_DESKTOP" = true ] && [ "$ID" = "arch" ]; then
+if [[ "$ID" = "arch" ]]; then
 	DESKTOP_PKGS=(
-		"brave-bin"
-		"brightnessctl"
-		"cronie"
-		"dos2unix"
-		"dracula-gtk-theme"
-		"fastfetch"
-		"feh"
-		"ghostty"
-		"htop"
-		"inetutils"
-		"ipython"
-		"man-db"
-		"networkmanager"
-		"newsboat"
-		"nwg-look"
-		"openssh"
-		"playerctl"
-		"python"
-		"signal-desktop"
-		"spotify"
-		"syncthing"
-		"ttf-jetbrains-mono-nerd"
-		"wiremix"
-		"yazi"
-		"zathura"
-		"zathura-pdf-poppler"
+        "brave-bin"
+        "brightnessctl"
+        "cronie"
+        "dos2unix"
+        "dracula-gtk-theme"
+        "dunst"
+        "fastfetch"
+        "feh"
+        "ghostty"
+        "htop"
+        "inetutils"
+        "ipython"
+        "man-db"
+        "networkmanager"
+        "newsboat"
+        "nwg-look"
+        "openssh"
+        "playerctl"
+        "python"
+        "signal-desktop"
+        "spotify"
+        "syncthing"
+        "ttf-jetbrains-mono-nerd"
+        "wiremix"
+        "yazi"
+        "zathura"
+        "zathura-pdf-poppler"
 	)
 
 	HYPRLAND_PKGS=(
@@ -56,4 +51,7 @@ if [ "$IS_DESKTOP" = true ] && [ "$ID" = "arch" ]; then
 	else
 		echo "Error: yay not found. Skipping desktop packages."
 	fi
+else
+    echo "Only Arch is supported at this time for desktop installation"
+    exit 1
 fi
