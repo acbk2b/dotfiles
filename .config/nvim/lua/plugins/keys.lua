e6a84d00d33aaf074cfc3c12bc5d9d8dd5fab9c0
Alex Becker <alex@beckerlab.net>
2026-09-25 20:19:39 -0500

    feat: drop tmux from shell startup

--- .config/nvim/lua/plugins/keys.lua
+++ .config/nvim/lua/plugins/keys.lua
@@ -9,25 +9,6 @@ return {
 			{ "<leader>f", "<cmd>Files<cr>", desc = "Find files (fzf)" },
 		},
 	},
-	-- Vim-style navigation with tmux
-	{
-		"christoomey/vim-tmux-navigator",
-		cmd = {
-			"TmuxNavigateLeft",
-			"TmuxNavigateDown",
-			"TmuxNavigateUp",
-			"TmuxNavigateRight",
-			"TmuxNavigatePrevious",
-			"TmuxNavigatorProcessList",
-		},
-		keys = {
-			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-		},
-	},
 	-- Change enclosing characters
 	{
 		"tpope/vim-surround",
